257bfa5c020d48ee0120f3509ee8a03978e768cd
keisuke3 <ishii@gizumo-inc.jp>
2026-03-17 16:32:13 +0900

    ユニオン型の練習問題追加

---
+++ src/lesson/unionTypes.ts
@@ -0,0 +1,30 @@
+// ここにコードを書きながら確認しましょう！
+
+
+
+
+
+
+
+
+
+// 練習問題
+// 問題 1
+function showScore(score) {
+  console.log(`スコア: ${score}`);
+}
+
+function showResult(result) {
+  console.log(`結果: ${result}`);
+}
+
+showScore(100);
+showScore('満点');
+
+showResult(true);
+showResult(null);
+
+// 問題 2
+const user1: UserProfile = { name: '太郎', age: 25, isActive: true };
+const user2: UserProfile = { name: '花子', age: '非公開', isActive: false };
+const user3: UserProfile = { name: '次郎', age: 30, isActive: null };
