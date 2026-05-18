45343677738f891fb520721d0102aa0a700cf676
horike <horike@Gizumo-admin-38noMacBook-Air.local>
2026-05-18 19:40:24 +0900

    add: DOMOparation

---
+++ src/DOMOparation-lesson/DOMOparation.ts
@@ -0,0 +1,18 @@
+console.log("DOM操作の練習問題");
+// ここにコードを書きながら確認しましょう！
+
+
+
+
+
+// 練習問題
+const element = document.getElementById('email-input');
+
+function printEmailValue() {
+
+  
+  // ここに回答を記述
+  
+}
+
+printEmailValue();
