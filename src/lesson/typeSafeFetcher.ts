0e4dd832e0881f1bda400a3e6abefa6f32b8ffe4
taipeijapan <bumpofchelsea0910@gmail.com>
2026-05-27 15:56:11 +0900

    add: typeSafeFetcher

---
+++ src/lesson/typeSafeFetcher.ts
@@ -0,0 +1,23 @@
+// Userインターフェースの定義
+interface User {
+  // プロパティを記述
+}
+
+// User 型であるか判定する型ガード関数
+function isUser(item) {
+  // 処理内容を記述   
+}
+
+// User[] 型であるか判定する型ガード関数
+function isUserArray(data) {
+  // 処理内容を記述 
+}
+
+// データを取得して加工するメイン関数
+async function fetchAndFilterUsers() {
+  const url: string = "https://jsonplaceholder.typicode.com/users";
+
+  // 処理内容を記述   
+}
+
+fetchAndFilterUsers();
