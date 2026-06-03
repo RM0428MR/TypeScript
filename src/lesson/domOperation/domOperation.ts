ae91c8f1171a3adedd86c029379ac1a6f06f3792
keisuke3 <ishii@gizumo-inc.jp>
2026-06-03 20:06:00 +0900

    練習問題の格納場所を変更

---
+++ src/lesson/domOperation/domOperation.ts
@@ -0,0 +1,20 @@
+// ここにコードを書きながら確認しましょう！
+
+
+
+
+
+
+
+// 練習問題
+// 問題 1
+const element = document.getElementById('email-input') as HTMLInputElement;
+console.log(element.value);
+
+
+// 問題 2
+function printEmailValue() {
+  const element = document.getElementById('email-input');
+}
+
+printEmailValue();
