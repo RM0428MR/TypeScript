198225d91c481811e3037b942621b76af3ffb110
keisuke3 <ishii@gizumo-inc.jp>
2026-06-09 12:24:39 +0900

    練習問題修正

---
+++ src/lesson/chatApp/eventHandler.ts
@@ -0,0 +1,21 @@
+import { sendMessage } from './chat.js';
+
+// ここにコードを書きながら確認しましょう！
+
+
+
+
+
+
+
+
+
+
+// 練習問題
+// 問題 1
+const sendButton = document.getElementById('send-btn');
+if (sendButton) {
+
+}
+
+// 問題 2
