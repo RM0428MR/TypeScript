15c395ed53607e26b517cb10d47a2d1c49532ebe
RM0428MR <breakin@gmail.com>
2026-08-20 17:21:36 +0900

    Section4

--- src/lesson/arrayAndTuple.ts
+++ src/lesson/arrayAndTuple.ts
@@ -10,19 +10,19 @@
 
 // 練習問題
 // 問題 1
-const memberNames = [];
+const memberNames: string[] = [];
 memberNames.push('太郎', '花子', '次郎');
 
-const scores = [];
+const scores: number[] = [];
 scores.push(85, 92, 78);
 
-const isAvailable = [];
+const isAvailable: boolean[] = [];
 isAvailable.push(true, false, true);
 
 // 問題 2
-const player1 = ['太郎', 3500, true];
-const player2 = ['花子', 4200, false];
-const player3 = ['次郎', 2800, true];
+const player1: [string, number, boolean] = ['太郎', 3500, true];
+const player2: [string, number, boolean] = ['花子', 4200, false];
+const player3: [string, number, boolean] = ['次郎', 2800, true];
 
 const bonusScore1 = player1[1] + 100;
 const bonusScore2 = player2[1] + 100;
